d248663983abd08f00a8cda0582ff445b499869f
glyn-stevens <glynst@gmail.com>
2024-02-12 00:15:28 +0000

    add ruff format to lint

--- bin/lint.ps1
+++ bin/lint.ps1
@@ -1,7 +1,9 @@
 $TARGER_DIR="src"
 
 "`nruff:"
+poetry run ruff format $TARGER_DIR
 poetry run ruff --fix $TARGER_DIR
+
 
 "`nmypy tests:"
 poetry run mypy $TARGER_DIR --ignore-missing-imports --check-untyped-defs
