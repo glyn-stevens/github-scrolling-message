bb73e4c7bed1fc1120e285ae8c70408f665f826b
glyn-stevens <glynst@gmail.com>
2024-08-02 00:08:30 +0100

    improve msgs

--- tests/lambda_func/test_lambda_func.py
+++ tests/lambda_func/test_lambda_func.py
@@ -9,9 +9,8 @@ from lambda_func.lambda_function import commit_on_day
 def test_get_pixels_string_for_day(days: int, encoded_array: np.ndarray):
     pixel_string = pixel_array_to_string(encoded_array, days + 1)
     assert len(pixel_string.splitlines()) == 7
-    assert (
-        len(pixel_string) == days + 1 + 7
-    ), "Pixel string should have one char per day (inc. day 0), plus 7 new line chars"
+    msg = "Pixel string should have one char per day (inc. day 0), plus 7 new line chars"
+    assert len(pixel_string) == days + 1 + 7, msg
     assert all(c in [MSG_FILLED_PIXEL, MSG_EMPTY_PIXEL, "\n"] for c in pixel_string)
 
 
@@ -23,9 +22,8 @@ def test_commit_on_day():
     expected_values = [True, False, True, False, True, False, True, False, True]
 
     for idx, expected in enumerate(expected_values):
-        assert (
-            commit_on_day(TEST_ARRAY, idx) == expected
-        ), f"Expected {expected}, at index {idx}"
+        msg = f"Expected {expected}, at index {idx}"
+        assert commit_on_day(TEST_ARRAY, idx) == expected, msg 
 
 
 def test_commit_on_day_out_of_range():
